9ca019d4ede53425a9c622dfdeaa015377f5098c
yitshak <yitshak.yarom@gmail.com>
2022-05-11 08:34:23 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>This is my heading</h1>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
